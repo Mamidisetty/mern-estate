8f688afba80d24b5adacbec931ed15247b6255fc
Mamidisetty <127787361+Mamidisetty@users.noreply.github.com>
2023-10-24 12:25:09 +0530

    create sign-in api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import {Link,useNavigate} from 'react-router-dom'
+import { useState } from 'react';
+import {Link,useNavigate} from 'react-router-dom';
 
 export default function SignUp() {
   const [formData,setFormData]=useState({});
@@ -25,9 +25,10 @@ export default function SignUp() {
       body:JSON.stringify(formData),
     });
     const data = await res.json();
+    console.log(data);
     if(data.success === false){
-      setError(data.message);
       setLoading(false);
+      setError(data.message);
       return;
     }
     setLoading(false);
